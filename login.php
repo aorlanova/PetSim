82f70b6649a6adb00afd29f278d0c5746a6147d0
Ari Key <zenzeleinbox@gmail.com>
2023-11-10 19:04:09 -0500

    Created login and registration page

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <body>
+            <div class="container">
+                <h1>Login</h1>
+                <form action="">
+                    <input type="text" name="Username" placeholder="Username">
+                    <input type="text" name="password" placeholder="Password">
+                    <input type="submit" value="Login" name="login-button">
+                </form>
+            </div>
+        </body>
+    </head>
+</html>
---
+++ registration.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <body>
+            <div class="container">
+                <h1>Register</h1>
+                <form action="">
+                    <input type="text" name="Username" placeholder="Username" maxlength="16">
+                    <input type="text" name="name" placeholder="Name">
+                    <input type="text" name="age" placeholder="Age" maxlength="2">
+                    <input type="text" name="password" placeholder="Password">
+                    <input type="submit" value="Register" name="login-button">
+                </form>
+            </div>
+        </body>
+    </head>
+</html>
